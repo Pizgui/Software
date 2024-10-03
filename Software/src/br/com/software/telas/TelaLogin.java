2adc199ad7ac6e3dc2cadd36298d7cacca708bf2
Guilherme <Guilherme@DESKTOP-S052R7T>
2024-10-03 16:47:19 -0300

    Adicionei outras telas

--- Software/src/br/com/software/telas/TelaLogin.java
+++ Software/src/br/com/software/telas/TelaLogin.java
@@ -195,8 +195,6 @@ public class TelaLogin extends javax.swing.JFrame {
         }
         //</editor-fold>
         //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(() -> {
